3223b91de7c01b01fbb9a85ce17eae3e44d8dce6
UPCoders <antoniocontrabaix@gmail.com>
2020-04-12 16:51:51 +0200

    app gunicorn

--- flask_try.py
+++ flask_try.py
@@ -4,9 +4,7 @@ from flask import Flask, jsonify
 #from json import jsonify
 app = Flask(__name__)
 
-tipus = input("Donam un tipus de comperç: ")
-tipus2 = input("Dona'm un tipus de comerç: ")
-activities=[tipus, tipus2]
+activities=['Fruiteries i verduleries', 'Carnisseries']
 home_coords=Coords(x_utm=430226.85, y_utm=4581590.93)
 pf = PathFinder()
 

296b8b32964ae5fcfac79962545b37fc5ec9209b
UPCoders <antoniocontrabaix@gmail.com>
2020-04-12 19:50:14 +0200

    app1.0.2

--- flask_try.py
+++ flask_try.py
@@ -4,14 +4,15 @@ from flask import Flask, jsonify, request
 #from json import jsonify
 app = Flask(__name__)
 
-content = request.get_json(silent=True)
-activities = []
-for activity in content['activities']:
-    activities.append(activity)
 
-home_coords=Coords(latitude=content['latitude'], longitude=content['longitude'])
 pf = PathFinder()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def execute_smartshopping():
+    content = request.get_json(silent=True)
+    activities = []
+    for activity in content['activities']:
+        activities.append(activity)
+
+    home_coords = Coords(latitude=content['latitude'], longitude=content['longitude'])
     return jsonify(pf.find_best_path(desired_activities=activities, my_coords=home_coords))
